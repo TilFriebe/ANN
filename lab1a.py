192fcd1344d84acb368ab5d4229901cd4a284b56
David <david.ye01@hotmail.com>
2023-01-23 11:13:36 +0100

    lab 1a, fixing errors, matrices

--- lab1a.py
+++ lab1a.py
@@ -1,80 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def main():
-    n, bias, eta, epochs = 100, 0, [0.01], 5
+    n, bias, sigmaW, epochs = 100, 1, 0.1, 5
+
+
+    weights = np.random.standard_normal(3) * sigmaW
 
     #mean and standard deviation set A
-    mA = [-3, -3]
+    mA = [-2, 0]
     sigmaA = 0.3
 
     #mean and standard deviation set B
-    mB = [3, 3]
+    mB = [-6, 0]
     sigmaB = 0.3
-
-    '''
-    Weights standard deviation, integers?
-    
-    '''
-    #standard deviation Weights
-    sigmaW = 4
-    dimension = 2
-    weights = InitialWeightMatrix(dimension, sigmaW, bias)
 
     #create the sets, each containing n elements
     classA1 = np.random.standard_normal(n) * sigmaA + mA[0]
-    # For 3.1.3
-    # classA1 = np.append(np.random.standard_normal(n/2) * sigmaA + mA[0], np.random.standard_normal(n/2) * sigmaA - mA[0])
     classA2 = np.random.standard_normal(n) * sigmaA + mA[1]
     classB1 = np.random.standard_normal(n) * sigmaB + mB[0]
     classB2 = np.random.standard_normal(n) * sigmaB + mB[1]
 
     A, B = np.vstack((classA1, classA2)), np.vstack((classB1, classB2))
 
+    #learning rates
+    eta = [0.0001, 0.001, 0.01, 0.1]
+
     #all errors and weights for different eta
     allErrors = []
     allWeights = []
 
-    #plot graph
-    plt.figure()
-
-
-    plt.plot(classA1, classA2, 'x')
-    plt.plot(classB1, classB2, 'x')
-
-    #to plot the initial hyperplane (red)
-    plt.arrow((bias/np.dot(weights,weights))*weights[0], (bias/np.dot(weights,weights))*weights[1], weights[0], weights[1], head_width=0.2, color="red")
-    x=np.linspace(-4,4)
-    plt.plot(x,(bias-weights[0]*x)/weights[1], '-r')
+    learning_methods = [perceptron_learning]
 
     #looping over all eta
-    learning_methods = [perceptron_learning, delta_learning_online, delta_learning_batch]
     for i in eta:
-        data = perceptron(A, B, n, weights, bias, learning_methods, epochs, i, False)
-        allErrors.append(data[1])
+        data = perceptron(A, B, n, weights, learning_methods, i, epochs)
         allWeights.append(data[0])
-    print("all errors ", allErrors, "\n")
-    weights1 = allWeights[0][0]
-
-    print("print SHAPE OF ERRORS", np.shape(allErrors), "\n")
-    print("print allErrors before plot: ", allErrors)
+        allErrors.append(data[1])
 
 
-
-
-
+    # plot graph
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.suptitle('Horizontally stacked subplots')
 
-    #Plot 1
-    error1 = allErrors[0][0]
+    # Plot 1
+    # calculating errors
+    xList = [i for i in range(1, epochs + 1)]
 
     plt.subplot(1, 2, 2)
-    plt.plot(error1, 'black')
+    plt.plot(xList, allErrors[0][0])
+    plt.legend(("Perceptron learning", "Delta rule online", "Delta rule batch"))
 
     plt.ylabel('errors')
     plt.xlabel('epochs')
 
-    #Plot 2
+    # Plot 2
     plt.subplot(1, 2, 1)
 
     plt.plot(classA1, classA2, 'x', color="green")
@@ -86,117 +65,77 @@ def main():
     x = np.linspace(-4, 4)
     plt.plot(x, (bias - weights[0] * x) / weights[1], '-r', linewidth=1.5)
 
-    #Hyperplane after epochs
-    plt.arrow((bias / np.dot(weights1, weights1)) * weights1[0], (bias / np.dot(weights1, weights1)) * weights1[1],
-              weights1[0], weights1[1], head_width=0.2, color="green")
-    x = np.linspace(-4, 4)
-    plt.plot(x, (bias - weights1[0] * x) / weights1[1], '--b')
+    plot_map = ["blue", "orange", "green", "--b", "--o", "--g"]
 
+    # Hyperplane after epochs
+    for i in range(len(learning_methods)):
+        plt.arrow((allWeights[0][i][2]*bias / np.dot(allWeights[0][i], allWeights[0][i])) * allWeights[0][i][0],
+                  (allWeights[0][i][2]*bias / np.dot(allWeights[0][i], allWeights[0][i])) * allWeights[0][i][1],
+                  allWeights[0][i][0], allWeights[0][i][1], head_width=0.2, color=plot_map[i])
+        x = np.linspace(-4, 4)
+        plt.plot(x, (allWeights[0][i][2]*bias - allWeights[0][i][0] * x) / allWeights[0][i][1], plot_map[3 + i])
 
     plt.show()
 
-def perceptron(classPos, classNeg, n, weights, bias, learning_methods, epochs, eta = 1, batch = True):
-    """
-
-    :param classPos1: x values for class 1
-    :param classPos2: y values for class 1
-    :param classNeg1: x values for class 2
-    :param classNeg2: y values for class 2
-    :param n: number of data points in each class
-    :param weights: weights of the linear line
-    :param bias: A shifting parameter to move it away from origin
-    :param learning_method: choose either perceptron learning or delta rule
-    :param eta: learning rate
-    :param batch: choose either batch learning or on-line earning
-    :return: weights and errors
-    """
-    #
-    classes = np.vstack((np.hstack((classNeg, classPos)), np.array((2*n)*[1])))
+def perceptron(classPos, classNeg, n, weights, learning_methods, eta, epochs):
+    classes = np.vstack((np.hstack((classNeg, classPos)), np.array((2 * n) * [1])))
     allWeights = [weights.copy() for i in range(len(learning_methods))]
     allErrors = [[] for i in range(len(learning_methods))]
-
 
     for i in range(epochs):
         # deciding order of samples. Neg: 1 to n, Pos: n+1 to 2n
         orderSamples = np.random.permutation(2 * n)
         for j in range(len(learning_methods)):
-            variables = learning_methods[j](n, classes, allWeights[j], eta, orderSamples)
-            allWeights[j] = variables[0]
-            allErrors[j].append(nErrors(variables[0], variables[1], classes))
-    print(allWeights)
-    '''
-    for i in orderSamples:
-        #t is the real value of the sample, x is the sample
-        variables = learning_method(n, i, classes, weights, bias)
-        t = variables[0]
-        x = variables[1]
-        y = variables[2]
-        #update weights
-        errors.append(t-y)
-        if batch:
-            weights_temp = np.add(weights_temp, np.dot(t-y, x))
-        else:
-            weights=np.add(weights,eta*np.dot(t-y,x))
-    if batch:
-        weights += eta*weights_temp
-    '''
-    # to plot the new hyperplanes (green)
-    """
-    plt.arrow((bias / np.dot(weights, weights)) * weights[0], (bias / np.dot(weights, weights)) * weights[1],
-              weights[0], weights[1], head_width=0.2, color="green")
-    x = np.linspace(-4, 4)
-    plt.plot(x, (bias - weights[0] * x) / weights[1], '-g')
-    """
+            learning_methods[j](n, classes, allWeights[j], eta, orderSamples, allErrors[j])
+
 
     return [allWeights, allErrors]
 
-def perceptron_learning(n, i, classes, weights, bias):
-    """
+def perceptron_learning(n, classes, weights, eta, permutation, error):
+    totError = 0
+    for i in permutation:
+        t = i // n
+        x = classes[:, i]
 
-    :param n: number of data points in each class
-    :param i: index of a certain Data point
-    :param classes: A list of classes including their coordinates
-    :param weights: weights of the linear line
-    :param bias: A shifting parameter to move it away from origin
-    :return: The target vakue t, the coordinates for the data point x and the estimated target value y
-    """
-    t = (i - 1) // n
-    x = [classes[t][0][(i - 1) % n], classes[t][1][(i - 1) % n]]
-    y = threshold(np.dot(weights, x), bias)
-    return [t, x, y]
+        y = threshold(np.dot(weights[:2], x[:2]), weights[2])
+        weights += eta * np.dot((t - y), x)
+        totError += check_errors(weights, n, classes, i)
+    error.append(totError)
 
-def delta_rule(n, i, classes, weights, bias):
-    """
+def delta_learning_online(n, classes, weights, eta, permutation, error):
+    totError = 0
+    for i in permutation:
+        t = (i // n) * 2 - 1
+        x = classes[:, i]
+        y = np.dot(weights, classes[:, i])
+        weights += eta * np.dot((t - y), x)
+        totError += check_errors(weights, n, classes, i)
+    error.append(totError)
 
-    :param n: number of data points in each class
-    :param i: index of a certain Data point
-    :param classes: A list of classes including their coordinates
-    :param weights: weights of the linear line
-    :param bias: A shifting parameter to move it away from origin
-    :return: The target vakue t, the coordinates for the data point x and the estimated target value y
-    """
+def delta_learning_batch(n, X, W, eta, permutation, error):
+    totError = 0
+
+    for i in permutation:
+        totError += check_errors(W, n, X, i)
+    T = np.array(n * [-1] + n * [1])
+    W += eta * np.dot(T - np.dot(W, X), np.transpose(X))
+    error.append(totError)
 
 
-def threshold(y):
-    """
 
-    :param y: A number representing whether a data point is in the correct classification
-    :param bias: A shifting parameter to move it away from origin
-    :return: The estimated target value
-    """
-    if y > 0:
+def threshold(y, bias):
+    if y > bias:
         return 1
     return 0
 
-def InitialWeightMatrix(dimension, sigmaW, bias):
-    return np.append(np.random.standard_normal(dimension) * sigmaW,[-bias])
+
+def check_errors(W, n, X, i):
+    error = 0
+    norm = np.sqrt(np.dot(W, W))
+    projOnW = (np.dot(X[:, i], W) / norm ** 2) * W
+    if ((np.dot(projOnW, W) > 0 and i < n) or (np.dot(projOnW, W) <= 0 and i > n - 1)):
+        error = 1
+    return error
 
 if __name__ == '__main__':
     main()
-
-
-'''
-Epochs
-Convergence
-Bias delta rule
-'''
